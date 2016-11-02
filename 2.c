c5c8ad501c0b960ba3abc1e2f1eab028af46d2a7
Borais <lyonya.kizhner@yandex.ru>
2016-11-03 00:18:41 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -3,26 +3,26 @@
 
 
 /**
-* double binaryToDecimal функция для перевода из двоичной в десятичную СС
-* @param double binary двоичное число, которое надо перевести
-* @var double binaryNumber - статичное двочиное число для рекурсии
-* @var int degree - статичная степень для перевода
-* @var double decimalNumber - статичное десятичное число, которое нужно вычислить
-* @var double modfHelper - поможет при вычислении разряда
+* double binaryToDecimal С„СѓРЅРєС†РёСЏ РґР»СЏ РїРµСЂРµРІРѕРґР° РёР· РґРІРѕРёС‡РЅРѕР№ РІ РґРµСЃСЏС‚РёС‡РЅСѓСЋ РЎРЎ
+* @param double binary РґРІРѕРёС‡РЅРѕРµ С‡РёСЃР»Рѕ, РєРѕС‚РѕСЂРѕРµ РЅР°РґРѕ РїРµСЂРµРІРµСЃС‚Рё
+* @var double binaryNumber - СЃС‚Р°С‚РёС‡РЅРѕРµ РґРІРѕС‡РёРЅРѕРµ С‡РёСЃР»Рѕ РґР»СЏ СЂРµРєСѓСЂСЃРёРё
+* @var int degree - СЃС‚Р°С‚РёС‡РЅР°СЏ СЃС‚РµРїРµРЅСЊ РґР»СЏ РїРµСЂРµРІРѕРґР°
+* @var double decimalNumber - СЃС‚Р°С‚РёС‡РЅРѕРµ РґРµСЃСЏС‚РёС‡РЅРѕРµ С‡РёСЃР»Рѕ, РєРѕС‚РѕСЂРѕРµ РЅСѓР¶РЅРѕ РІС‹С‡РёСЃР»РёС‚СЊ
+* @var double modfHelper - РїРѕРјРѕР¶РµС‚ РїСЂРё РІС‹С‡РёСЃР»РµРЅРёРё СЂР°Р·СЂСЏРґР°
 * @return decimalNumber
 */
 
-double binaryToDecimal(double binaryNumber)
+double binaryToDecimal( double binaryNumber )
 {
-	static int degree = 0;
-	static double decimalNumber = 0;
-	double modfHelper = 0;
+    static int degree = 0;
+    static double decimalNumber = 0;
+    double modfHelper = 0;
 
-	binaryNumber /= 10;
-	modfHelper = modf(binaryNumber, &binaryNumber);
-	modfHelper *= 10;
-	decimalNumber += modfHelper * pow(2, degree);
-	degree++;
+    binaryNumber /= 10;
+    modfHelper = modf( binaryNumber, &binaryNumber );
+    modfHelper *= 10;
+    decimalNumber += modfHelper * pow( 2, degree );
+    degree++;
 	if (modfHelper == 0 || modfHelper == 1)
 	{
 		if (binaryNumber == 0)
@@ -34,13 +34,13 @@ double binaryToDecimal(double binaryNumber)
 		return -1;
 }
 
-int main(void)
+int main( void )
 {
-	double binaryNumber;
-	double decimalNumber;
+    double binaryNumber;
+    double decimalNumber;
 
-	scanf("%lf", &binaryNumber);
-	//Проверка на валдиность числа
+    scanf("%lf", &binaryNumber);
+	//РџСЂРѕРІРµСЂРєР° РЅР° РІР°Р»РґРёРЅРѕСЃС‚СЊ С‡РёСЃР»Р°
 	if (binaryNumber > 0)
 	{
 		decimalNumber = binaryToDecimal(binaryNumber);
@@ -48,11 +48,11 @@ int main(void)
 		if (decimalNumber != -1)
 			printf("%.0lf", decimalNumber);
 		else
-			printf("Введите корректное число!");
+			printf("Р’РІРµРґРёС‚Рµ РєРѕСЂСЂРµРєС‚РЅРѕРµ С‡РёСЃР»Рѕ!");
 
 	}
 	else
-		printf("Введите число больше нуля!");
+		printf( "Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ Р±РѕР»СЊС€Рµ РЅСѓР»СЏ!" );
 
-	return 0;
+    return 0;
 }
