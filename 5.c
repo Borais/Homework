f02636b539dce0daa369aa666279edd475cd34f9
Borais <lyonya.kizhner@yandex.ru>
2016-12-10 17:56:14 +0300

    Update 5.c

--- 5.c
+++ 5.c
@@ -9,27 +9,21 @@ int main()
 	scanf("%lf", &a);
 	scanf("%lf", &b);
 	scanf("%lf", &c);
-
 	D = b*b - 4 * a*c;
 	if (D > 0)
 	{
-
 		x1 = (-b + sqrt(D)) / (2 * a);
 		x2 = (-b - sqrt(D)) / (2 * a);
 		printf("X1 = %.3lf\n X2 = %.3lf\n", x1, x2);
-
 	}
 	else if (D == 0)
 	{
-
 		x1 = (-b + sqrt(D)) / (2 * a);
 		printf("X = %.3lf\n", x1);
-
 	}
 	else if (D < 0)
 	{
 		printf("The discriminant is less than zero\n");
 	}
-
 	return 0;
 }
