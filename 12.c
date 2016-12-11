49f38c8c56e1a52572e1a7b8fc57d6014d513bd3
Borais <lyonya.kizhner@yandex.ru>
2016-12-11 09:23:05 +0300

    Add files via upload

---
+++ 12.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_STRING_SIZE 255
+
+
+int numsLength;
+
+//initialization of original function
+int string_to_number(char *a){
+    int num = atoi(a);
+    return num;
+}
+
+
+void flush_string(char *a){
+    int i;
+    for (i = 0; i < MAX_STRING_SIZE; i++){
+        a[i] = '\0';
+    }
+}
+
+//initialization of converting function
+int* convert_string_to_numbers(char *a){
+    static int answ[MAX_STRING_SIZE];
+    int *p = answ, i, answLength = 0;
+    static char curNumberArr[MAX_STRING_SIZE];
+    char *curNumber = curNumberArr;
+    int iCurNumber;
+    int iCurNumberLength = 0;
+    for (i = 0; i < strlen(a); i++){
+        if (a[i] == '.'){
+            iCurNumber = string_to_number(curNumber);
+            answ[answLength] = iCurNumber;
+            answLength++;
+            break;
+        }
+        if (a[i] == ' '){
+            iCurNumber = string_to_number(curNumber);
+            answ[answLength] = iCurNumber;
+            answLength++;
+            flush_string(curNumber);
+            iCurNumberLength = 0;
+        } else {
+            curNumber[iCurNumberLength] = a[i];
+            iCurNumberLength++;
+        }
+    }
+
+    numsLength = answLength;
+    return p;
+}
+
+int main (int argc, char *argv[])
+{
+    char a[MAX_STRING_SIZE];
+    // output array of numbers
+    int *nums, i;
+    printf("Type a consequence on numbers with a dot on the end:\n");
+    gets(a);
+    nums = convert_string_to_numbers(a);
+    printf("\nOutput:\n");
+    for (i = 0; i < numsLength; i++){
+        if (i != numsLength)
+            printf("%d\t ", nums[i]);
+        else
+            printf("%d");
+        if ((i+1) % 3 == 0 && i != 0)
+            printf("\n");
+    }
+	return 0;
+}
